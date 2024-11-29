6bdd7f245b2a704e1845b26e0d1fb7278534318f
Lewis Hadden <lewishadden@gmail.com>
2024-11-29 15:12:58 +0000

    Whitelist Toast background styling

--- purgecss.config.cjs
+++ purgecss.config.cjs
@@ -3,6 +3,6 @@ module.exports = {
   css: ['out/_next/static/css/*.css'],
   output: 'out/_next/static/css',
   safelist: {
-    deep: [/modal$/, /modal-(.)*$/, /btn-close(.)*$/, /awssld/, /col-/],
+    deep: [/modal$/, /modal-(.)*$/, /btn-close(.)*$/, /awssld/, /col-/, /bg-(success|danger)$/],
   },
 };
