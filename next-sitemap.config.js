0c72bf44f6caadf8609538b58e928e1eb834ea2f
Lewis Hadden <lewishadden@gmail.com>
2026-02-22 16:00:06 +0000

    Remove robots.txt generation

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,31 +1,12 @@
 /** @type {import('next-sitemap').IConfig} */
 export default {
   siteUrl: process.env.NEXT_PUBLIC_SITE_URL || 'https://portfolio.lewishadden.com',
-  generateRobotsTxt: true,
+  generateRobotsTxt: false,
   changefreq: 'monthly',
   priority: 1.0,
   generateIndexSitemap: false,
   outDir: 'public',
   exclude: ['/server-sitemap.xml'],
-  robotsTxtOptions: {
-    policies: [
-      {
-        userAgent: '*',
-        allow: '/',
-      },
-      {
-        userAgent: 'Googlebot',
-        allow: '/',
-        crawlDelay: 0,
-      },
-      {
-        userAgent: 'Bingbot',
-        allow: '/',
-        crawlDelay: 0,
-      },
-    ],
-    additionalSitemaps: [],
-  },
   transform: async (config, path) => {
     // Custom priority and changefreq based on path
     if (path === '/') {
